338e1206b8f0b2e28ffdd92d17025ccdb181ba17
Hiroya Hinomori <nexearth@gmail.com>
2018-09-20 12:00:22 +0900

    Initial Commit

---
+++ TDDExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDDExample
+//
+//  Created by HINOMORI HIROYA on 2018/09/20.
+//  Copyright © 2018年 HINOMORI HIROYA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
